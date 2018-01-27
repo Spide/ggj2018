bba13698fe704257ba3da0116ed76781cb36236d
Ondřej Paška <on.paska@gmail.com>
2018-01-27 15:22:40 +0100

    wall jump double jump

--- Assets/scripts/movControl.cs
+++ Assets/scripts/movControl.cs
@@ -5,19 +5,29 @@ public class movControl : MonoBehaviour
 	
 	public float MaxSpeed;
 	public float JumpVelocity = 2;
+	public float DoubleJumpVelocity = 3;
 	
-	public bool _grounded;
+	private bool _grounded;
+	private bool _touchingWall;
+	
 	public Transform GroundCheck;
+	public Transform WallJumpCheckLeft;
+	public Transform WallJumpCheckRight;
 
-	private float groundRadius =  0.3f;
+	private float groundRadius =  0.1f;
+	private float wallJumpRadius =  0.2f;
 	public LayerMask WhatIsGround; 
 	private Rigidbody2D _rb;
 
 	private KeyCode _kUp;
 	private KeyCode _kDown;
 
-	public float FallMultiplier = 8f;
-	private float hasJumped;
+	public float FallMultiplier = 16f;
+	public float JumpMultiplier = 8f;
+	
+	private float _hasJumped;
+	private bool _hasWallJump;
+	private bool _doubleJumped;
 	
 	void Awake () {
 		_rb = GetComponent<Rigidbody2D> ();
@@ -29,44 +39,67 @@ public class movControl : MonoBehaviour
 	void Update()
 	{
 		_grounded = Physics2D.OverlapCircle (GroundCheck.position, groundRadius, WhatIsGround);
+		_touchingWall = Physics2D.OverlapCircle (WallJumpCheckLeft.position, wallJumpRadius, WhatIsGround);
+		if (!_touchingWall)
+		{
+			_touchingWall = Physics2D.OverlapCircle(WallJumpCheckRight.position, wallJumpRadius, WhatIsGround);
+		}
+		if (!_touchingWall || _grounded)
+		{
+			_hasWallJump = false;
+		}
+		if (_grounded)
+		{
+			_doubleJumped = false;
+		}
 	}
 	
 	void FixedUpdate(){
 		
 		
-		var move = Input.GetAxis ("Horizontal");
-		if (move > 0) {
-			_rb.velocity = new Vector2 (move * MaxSpeed, _rb.velocity.y);
-		} else if (move < 0) {
-			_rb.velocity = new Vector2 (move * MaxSpeed, _rb.velocity.y);
-		}else
-		{
-			_rb.velocity = new Vector2(0, _rb.velocity.y);
-		}
+		//HORIZONTAL MOVE
 		
+		var move = Input.GetAxisRaw("Horizontal");
+		var targetX = 0f;
+		if ( Mathf.Abs(move) > 0.05f )
+		{
+			targetX = Mathf.Lerp(_rb.velocity.x, move * MaxSpeed, 0.5f);
+		}
+		_rb.velocity = new Vector2(targetX, _rb.velocity.y);
+		
+		
+		// VERTICAL MOVE
+			
 		if (_rb.velocity.y < -0.05f)
 		{
-			print("fall");
-			hasJumped = 1;
-			_rb.velocity += Vector2.down*FallMultiplier*Time.deltaTime;
-			//_rb.velocity += Vector2.up * Physics2D.gravity.y * (FallMultiplier-1)*Time.deltaTime;
+			_hasJumped = 1;
+			_rb.velocity += Vector2.down *FallMultiplier * Time.deltaTime;
 		}
 		else if (_rb.velocity.y > 0.05f && Input.GetKey(_kUp))
 		{
-			_rb.velocity += Vector2.up*FallMultiplier*Time.deltaTime;
-			print("rise");
-			//_rb.velocity += Vector2.up * Physics2D.gravity.y * (LowJumpMultiplier-1)*Time.deltaTime;
+			_rb.velocity += Vector2.up*JumpMultiplier*Time.deltaTime;
 		}
 
-		if (_grounded && hasJumped > 0.4f && Input.GetKey(_kUp))
+		//JUMP
+		
+		if ( _grounded  && _hasJumped > 0.3f && Input.GetKey(_kUp))
 		{
-			print("FASDFASD");
-			hasJumped = 0;
+			_hasJumped = 0;
 			_rb.velocity = new Vector2 (_rb.velocity.x, JumpVelocity);
+		}else if (!_doubleJumped && _hasJumped > 0.1f && Input.GetKeyDown(_kUp))
+		{
+			_hasJumped = 0;
+			_doubleJumped = true;
+			_rb.velocity = new Vector2 (_rb.velocity.x, DoubleJumpVelocity);
+		}
+		else if (_touchingWall && !_hasWallJump && Input.GetKeyDown(_kUp))
+		{
+			_hasWallJump = true;
+			_rb.velocity = new Vector2(-_rb.velocity.x, JumpVelocity);
 		}
 		else
 		{
-			hasJumped += Time.deltaTime ;
+			_hasJumped += Time.deltaTime ;
 		}
 		
 	}
