3bc889e76db1b88e1b9c35f21ab4508c45f7e000
Ondřej Paška <on.paska@gmail.com>
2018-01-28 14:00:00 +0100

    intro

---
+++ Assets/introScreen/IntroScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScreen : MonoBehaviour
+{
+
+	public GameObject logo;
+	// Use this for initialization
+	void Start () {
+		logo.DOTween
+	}
+	
+	// Update is called once per frame
+	public void DOPLAY () {
+		SceneManager.LoadScene("arkanoid");
+		SceneManager.LoadScene("testscene",LoadSceneMode.Additive);
+	}
+}
